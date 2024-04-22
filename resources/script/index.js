ce0fa1fc411531d17ea2cad049f75345c51fb6cc
yeonmitc <>
2024-04-22 19:13:45 +0900

    update index.html
    .

--- resources/script/index.js
+++ resources/script/index.js
@@ -1,32 +1,34 @@
-const projectsSection = document.querySelector(".projects");
-const interestsSection = document.querySelector(".interests");
+const headers = [...document.getElementsByTagName("header")];
 
-const backendDiv = projectsSection.querySelector(".backend");
-const frontendDiv = projectsSection.querySelector(".frontend");
-
-const headers = document.getElementsByTagName("header");
-
-for (let i = 0; i < headers.length; i++) {
-  const header = headers[i];
-
+headers.forEach((header) => {
   const tags = header.getElementsByClassName("tag");
   const projectTag = tags[0];
-  const interestsTag = tags[1];
+  const myInfosTag = tags[1];
 
-  projectsSection.addEventListener("mouseenter", (e) => {
-    projectTag.setAttribute("style", "display : block");
-  });
-  projectsSection.addEventListener("mouseleave", (e) => {
-    projectTag.setAttribute("style", "display : none");
-  });
+  const mySections = document.querySelectorAll(".showTags");
+  mySections.forEach((section) => {
+    if (section.dataset.value === "projects") {
+      section.addEventListener("mouseenter", (e) => {
+        projectTag.setAttribute("style", "display : block");
+      });
 
-  interestsSection.addEventListener("mouseenter", (e) => {
-    interestsTag.setAttribute("style", "display : block");
+      section.addEventListener("mouseleave", (e) => {
+        projectTag.setAttribute("style", "display : none");
+      });
+    } else {
+      section.addEventListener("mouseenter", (e) => {
+        myInfosTag.setAttribute("style", "display : block");
+      });
+
+      section.addEventListener("mouseleave", (e) => {
+        myInfosTag.setAttribute("style", "display : none");
+      });
+    }
   });
-  interestsSection.addEventListener("mouseleave", (e) => {
-    interestsTag.setAttribute("style", "display : none");
-  });
-}
+});
+
+const backendDiv = document.querySelector(".backend");
+const frontendDiv = document.querySelector(".frontend");
 
 function Project(link, imageUrl, title, description, skills, period) {
   this.link = link;
